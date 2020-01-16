05142a1648fe67db5736e4afa58b034e4931f975
Topher Pedersen <chris@topherpedersen.com>
2020-01-15 19:54:21 -0600

    now pinging websocket echo endpoint from 3rd party with success

--- App.js
+++ App.js
@@ -11,9 +11,38 @@ import {
 class App extends React.Component {
   constructor(props) {
     super(props);
+
   }
 
   render() {
+
+    //alert("this thing working?");
+    var ws = new WebSocket('ws://echo.websocket.org/');
+
+    ws.onopen = () => {
+      // connection opened
+      alert("ws.onopen called!");
+      ws.send('something');
+    }
+
+    ws.onmessage = (e) => {
+      // a message was received
+      // alert(e.data.toString())
+      alert("ws.onmessage called!");
+    }
+
+    ws.onerror = (e) => {
+      // an error occurred
+      //alert(e.message.toString());
+      alert("ws.onerror called!");
+    }
+
+    ws.onclose = (e) => {
+      // connection closed
+      //alert(e.code.toString() + e.reason.toString());
+      alert("ws.onclose called!");
+    }
+
     return(
       <View>
         <Text>hello, world</Text>
